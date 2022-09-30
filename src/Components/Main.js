a7fe84825bc64d52c6097a2dddb9cd4adaad191c
Abby Benjamin <ablizben@gmail.com>
2022-09-29 21:26:12 -0400

    adding what landing page will look like minus image

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,9 +4,16 @@ import React from 'react';
 const Main = () => {
     return (
         <div className='home-page'>
-            <div>frontend developer | ui/ux engineer</div>
-            <div>about | projects | contact</div>
+            <div className="heading">frontend developer | ui/ux engineer</div>
+            <div className="line"></div>
+                <div className="body-div">
+                <div>hi, i'm abby.</div>
+                <div>image here</div>
+            </div>
+            <div className="line"></div>
+            <div className="nav">about | projects | contact</div>
         </div>
+
     )
 }
 
