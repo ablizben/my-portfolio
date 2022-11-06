4dd4afee8d95b1fd3f72201ff19c28922b3390a3
Abby Benjamin <ablizben@gmail.com>
2022-11-06 11:44:35 -0500

    editing text in about me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,7 +5,7 @@ const AboutMe = () => {
     return (
         <div>
             <div className="aboutme-summary">
-                i'm currently a ux/ui engineer at a cloud platform company. but whatever title you want to give me, I just enjoy building cool stuff and collaborating with others who do the same.  
+                i'm currently a ux/ui engineer at a cloud platform company. I just enjoy building cool stuff and collaborating with others who do the same.  
                 <br />
                 <br />
                 in my spare time, you can find me watching horror flicks and teaching my dog new tricks. 
